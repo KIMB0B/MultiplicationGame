55ba164bce26ce71a7aca9ccd24a29b76599b014
KIMB0B <axzsd96301@gmail.com>
2025-04-01 17:32:12 +0900

    feat: MultiplicationController에서 랜덤한 Multiplication을 응답하는 엔드포인트 추가

--- src/main/java/com/kimbob/multiplication/controller/MultiplicationController.java
+++ src/main/java/com/kimbob/multiplication/controller/MultiplicationController.java
@@ -1,12 +1,21 @@
 package com.kimbob.multiplication.controller;
 
+import com.kimbob.multiplication.domain.Multiplication;
 import com.kimbob.multiplication.service.MultiplicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/multiplications")
 @RequiredArgsConstructor
 public class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
+
+    @GetMapping("/random")
+    Multiplication getRandomMultiplication() {
+        return multiplicationService.createMultiplication();
+    }
 }
