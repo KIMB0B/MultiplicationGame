39e5427d39b28ec97e399bf89b2a3fe6fdd4db8c
KIMB0B <axzsd96301@gmail.com>
2025-04-02 15:07:11 +0900

    feat: Result 반환 API  엔드포인트 추가

--- src/main/java/com/kimbob/multiplication/controller/MultiplicationResultAttemptController.java
+++ src/main/java/com/kimbob/multiplication/controller/MultiplicationResultAttemptController.java
@@ -1,7 +1,12 @@
 package com.kimbob.multiplication.controller;
 
+import com.kimbob.multiplication.domain.MultiplicationResultAttempt;
+import com.kimbob.multiplication.domain.ResultResponse;
 import com.kimbob.multiplication.service.MultiplicationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,10 @@ public class MultiplicationResultAttemptController {
 
     private final MultiplicationService multiplicationService;
 
-
+    @PostMapping
+    public ResponseEntity<ResultResponse> postResult(@RequestBody MultiplicationResultAttempt multiplicationResultAttempt) {
+        return ResponseEntity.ok(
+                new ResultResponse(multiplicationService.checkAttempt(multiplicationResultAttempt))
+        );
+    }
 }
