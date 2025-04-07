e7970e4e3680b31184ea00e91a6d32e9261fc413
KIMB0B <axzsd96301@gmail.com>
2025-04-07 10:21:31 +0900

    feat: 계산 후 결과를 저장하는 로직 추가

--- src/main/java/com/kimbob/multiplication/service/MultiplicationServiceImpl.java
+++ src/main/java/com/kimbob/multiplication/service/MultiplicationServiceImpl.java
@@ -2,15 +2,22 @@ package com.kimbob.multiplication.service;
 
 import com.kimbob.multiplication.domain.Multiplication;
 import com.kimbob.multiplication.domain.MultiplicationResultAttempt;
+import com.kimbob.multiplication.domain.User;
+import com.kimbob.multiplication.repository.MultiplicationResultAttemptRepository;
+import com.kimbob.multiplication.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MultiplicationServiceImpl implements MultiplicationService {
 
     private final RandomGeneratorService randomGeneratorService;
+    private final UserRepository userRepository;
+    private final MultiplicationResultAttemptRepository attemptRepository;
 
     @Override
     public Multiplication createMultiplication() {
@@ -21,18 +28,26 @@ public class MultiplicationServiceImpl implements MultiplicationService {
 
     @Override
     public boolean checkAttempt(MultiplicationResultAttempt resultAttempt) {
+        // 해당 닉네임의 사용자가 존재하는지 확인
+        Optional<User> user = userRepository.findByAlias(resultAttempt.getUser().getAlias());
+
+        // 조작된 답안을 방지
+        Assert.isTrue(!resultAttempt.isCorrect(), "채점한 상태로 보낼 수 없습니다!");
+
+        // 답안 채점
         boolean correct = resultAttempt.getResultAttempt() ==
                 resultAttempt.getMultiplication().getFactorA() *
                         resultAttempt.getMultiplication().getFactorB();
 
-        Assert.isTrue(!resultAttempt.isCorrect(), "채점한 상태로 보낼 수 없습니다!");
-
         MultiplicationResultAttempt checkedAttempt = new MultiplicationResultAttempt(
-                resultAttempt.getUser(),
+                user.orElse(resultAttempt.getUser()),
                 resultAttempt.getMultiplication(),
                 resultAttempt.getResultAttempt(),
                 correct
         );
+
+        // 답안 저장
+        attemptRepository.save(checkedAttempt);
 
         return correct;
     }
--- src/test/java/com/kimbob/multiplication/service/MultiplicationServiceTest.java
+++ src/test/java/com/kimbob/multiplication/service/MultiplicationServiceTest.java
@@ -3,6 +3,8 @@ package com.kimbob.multiplication.service;
 import com.kimbob.multiplication.domain.Multiplication;
 import com.kimbob.multiplication.domain.MultiplicationResultAttempt;
 import com.kimbob.multiplication.domain.User;
+import com.kimbob.multiplication.repository.MultiplicationResultAttemptRepository;
+import com.kimbob.multiplication.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -12,10 +14,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 class MultiplicationServiceTest {
+
+    @Autowired
+    private MultiplicationResultAttemptRepository multiplicationResultAttemptRepository;
 
     @Autowired
     private RandomGeneratorService randomGeneratorService;
@@ -37,11 +43,12 @@ class MultiplicationServiceTest {
     void checkCorrectAttemptTest() {
         Multiplication multiplication = new Multiplication(50, 60);
         User user = new User("John_doe");
-        MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3000, true);
-
+        MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3000, false);
         boolean isCorrect = multiplicationService.checkAttempt(attempt);
 
         assertThat(isCorrect).isTrue();
+        MultiplicationResultAttempt checkedAttempt = new MultiplicationResultAttempt(user, multiplication, 3000, true);
+        verify(multiplicationResultAttemptRepository).save(checkedAttempt);
     }
 
     @Test
@@ -53,10 +60,21 @@ class MultiplicationServiceTest {
         boolean isCorrect = multiplicationService.checkAttempt(attempt);
 
         assertThat(isCorrect).isFalse();
+        verify(multiplicationResultAttemptRepository).save(attempt);
     }
 
     @TestConfiguration
     static class TestConfig {
+
+        @Bean
+        public UserRepository userRepository() {
+            return Mockito.mock(UserRepository.class);
+        }
+
+        @Bean
+        public MultiplicationResultAttemptRepository multiplicationResultAttemptRepository() {
+            return Mockito.mock(MultiplicationResultAttemptRepository.class);
+        }
 
         @Bean
         public RandomGeneratorService randomGeneratorService() {
@@ -64,8 +82,10 @@ class MultiplicationServiceTest {
         }
 
         @Bean
-        public MultiplicationService multiplicationService(RandomGeneratorService randomGeneratorService) {
-            return new MultiplicationServiceImpl(randomGeneratorService);
+        public MultiplicationService multiplicationService(RandomGeneratorService randomGeneratorService,
+                                                            UserRepository userRepository,
+                                                            MultiplicationResultAttemptRepository multiplicationResultAttemptRepository) {
+            return new MultiplicationServiceImpl(randomGeneratorService, userRepository, multiplicationResultAttemptRepository);
         }
     }
 }
